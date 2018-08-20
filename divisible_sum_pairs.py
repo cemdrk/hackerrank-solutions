eeb2343dd3882e873bceb29183b02cda4ea9f280
cemdrk <cemdurakgm@gmail.com>
2018-08-20 16:52:49 +0300

    divisible sum pairs

---
+++ divisible_sum_pairs.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+    result = 0
+    
+    for i in range(len(ar)-1):
+        for j in range(i+1, len(ar)):
+            if (ar[i] + ar[j]) % k == 0:
+                result += 1
+
+    return result
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
