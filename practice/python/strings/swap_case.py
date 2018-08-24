5462d3a2e17ffc830264de4d9c8c13a6fcda8799
cemdrk <cemdurakgm@gmail.com>
2018-08-24 10:37:24 +0300

    swap case

---
+++ practice/python/strings/swap_case.py
@@ -0,0 +1,15 @@
+
+def swap_case(s):
+    # result = ''
+    #
+    # for c in s:
+    #     if c.islower():
+    #         result += c.upper()
+    #     elif c.isupper():
+    #         result += c.lower()
+    #     else:
+    #         result += c
+    #
+    # print(result)
+
+    return s.swapcase()
