c54871d81b2656055a1b8ecd9488af6f28433eff
cemdrk <cemdurakgm@gmail.com>
2018-08-31 22:49:33 +0300

    weather observation station 11 single line

--- practice/sql/basicselect/weather_observation_station_11.sql
+++ practice/sql/basicselect/weather_observation_station_11.sql
@@ -2,9 +2,5 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
-select * from
-(
-    SELECT DISTINCT CITY FROM STATION WHERE REGEXP_LIKE(UPPER(CITY), '[^AEIOU]$')
-    UNION
-    SELECT DISTINCT CITY FROM STATION WHERE REGEXP_LIKE(UPPER(CITY), '^[^AEIOU]')
-);
+
+SELECT DISTINCT CITY FROM STATION WHERE REGEXP_LIKE(UPPER(CITY), '^[^AEIOU]|[^AEIOU]$');
