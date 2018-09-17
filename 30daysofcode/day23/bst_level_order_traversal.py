0bf430c9215bb5c9637bcfd35401317008067d6e
cemdrk <cemdurakgm@gmail.com>
2018-09-17 21:37:18 +0300

    BST Level-Order Traversal

---
+++ 30daysofcode/day23/bst_level_order_traversal.py
@@ -0,0 +1,38 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        q = []
+        q.append(root)
+
+        while len(q) > 0:
+            print(q[0].data, end=' ')
+            node = q.pop(0)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
