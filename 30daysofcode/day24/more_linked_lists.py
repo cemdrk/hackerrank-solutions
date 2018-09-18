2c7e0b3a8b9a606df159e605c6dbe368867837ee
cemdrk <cemdurakgm@gmail.com>
2018-09-18 21:01:21 +0300

    More Linked Lists

---
+++ 30daysofcode/day24/more_linked_lists.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def insert(self,head,data):
+            p = Node(data)
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        data_list = []
+        r = head
+        data_list.append(r.data)
+
+        c = r
+        n = c.next
+
+        while n:
+            data = n.data
+
+            if data in data_list:
+                c.next = n.next
+            else:
+                data_list.append(data)
+                c = c.next
+
+            n = c.next
+
+        return r
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+head=mylist.removeDuplicates(head)
+mylist.display(head)
